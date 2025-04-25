86a33efcaff4d6ea9aed2a04e9504a3f7ba68239
calebafework <calebafework@gmail.com>
2025-04-24 20:14:43 -0600

    Added /api/push-log and log storage

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+
+const { addLog, getLogs } = require('./services/LogStore');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Health check route
+app.get('/api/health', (req, res) => {
+  res.json({
+    status: 'OK',
+    uptime: process.uptime(),
+    message: 'All systems operational',
+    timestamp: new Date(),
+  });
+});
+
+// Logs route
+app.get('/api/logs', (req, res) => {
+    res.json(getLogs())
+});
+
+app.get('/api/push-log', (req, res) => {
+    const { type, message } = req.body;
+    if (!type || !message) {
+        return res.status(400).json({ error: 'Missing type or message' });
+    }
+
+    addLog({ type, message });
+    res.status(201).json({ status: 'Log added' });
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
